91f2c385b30ec10a7ca95170ad76321856146a76
margarine <rintanaru@proton.me>
2024-05-29 18:59:42 +0700

    idk

--- src/main/java/com/sateumami/controllers/ProductController.java
+++ src/main/java/com/sateumami/controllers/ProductController.java
@@ -1,11 +1,14 @@
 package com.sateumami.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +27,9 @@ public class ProductController {
 		return new ResponseEntity<List<Products>>(productService.allProducts(), HttpStatus.OK);
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<Optional<Products>> getSingleProduct(@PathVariable ObjectId id){
+		return new ResponseEntity<Optional<Products>>(productService.singleProduct(id), HttpStatus.OK);
+	}
 
 }
